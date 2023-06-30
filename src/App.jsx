d97f1c56d558a862d5c8221b7220547dfea15bfd
mranderson400 <mr_andersonr97@yahoo.com>
2023-06-30 14:56:09 -0400

    last update

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import '../src/index.css';
+import {About, Contact, Experience, Feedbacks,
+    Hero, Tech, Works, NavAll, StarsCanvas} from './components';
+
+// const StarsCanvasMemo = React.memo(StarsCanvas);
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="bg-cool-pattern bg-cover 
+          bg-repeat bg-center  ">
+        <div
+          className=" bg-cover 
+          bg-repeat bg-center "
+     
+        >
+        <NavAll/>
+          <Hero />
+        </div>
+        
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+           <Contact />
+          {/* <StarsCanvasMemo />  */}
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
