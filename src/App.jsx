d275f527731f79e401cac5c5424d3415a0ff85ea
mranderson400 <mr_andersonr97@yahoo.com>
2023-07-04 22:39:55 -0400

    hgg

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
      
         >
         <NavAll/>
-          <Hero />
+          {/* <Hero /> */}
         </div>
         {/* <ComputersCanvas/> */}
          {/* <About />  */}
