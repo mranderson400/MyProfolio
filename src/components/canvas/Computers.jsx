5c7ee1fa0cdbdf81dc6cde58931eb38bccc7cd35
mranderson400 <mr_andersonr97@yahoo.com>
2023-07-04 21:26:39 -0400

    new computer

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,17 +3,15 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
-const Computers = ({isMobile}) => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF('./desktop_pc/scene.gltf');
-  // guide to download 3d from sketchfab
-  // 1) Download , use the folder name as refrence and 
-  // import the scene.gltf as you would any other file
-
+  // Guide to download 3D model from Sketchfab
+  // 1) Download, use the folder name as reference
+  // 2) Import the scene.gltf as you would any other file
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15}
-       groundColor="black" />
+      <hemisphereLight intensity={0.15} groundColor="black" />
       <pointLight intensity={1} />
       <spotLight
         position={[-20, 50, 10]}
@@ -23,60 +21,48 @@ const Computers = ({isMobile}) => {
         castShadow
         shadow-mapSize={1024}
       />
-      <primitive object={computer.scene}
-        scale={ isMobile ? 0.1 : 0.30}
-
-        position={ isMobile ? 
-          [0, -3, -2.2] :
-          [0, -3.25, -1.5]
-          } />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.1 : 0.30}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+      />
     </mesh>
   );
 };
 
 const ComputersCanvas = () => {
-const [isMobile, setIsMobile] = useState(false);
-// the only thing useEffect is doing is changing the isMobile variable
+  const [isMobile, setIsMobile] = useState(false);
 
-useEffect(() => {
-  // we are using useEffect to check if someone on a mobile device by using mediaQuery
-  // Check if the user is on a mobile device using a media query
-  const mediaQuery = window.matchMedia('(max-width:500px)');
+  useEffect(() => {
+    // We are using useEffect to check if someone is on a mobile device using mediaQuery
+    const mediaQuery = window.matchMedia('(max-width:500px)');
 
-  setIsMobile(mediaQuery.matches);
-  // Update the isMobile state with the initial media query match status
-  // The setMobile function updates the isMobile state with this value.
+    // Update the isMobile state with the initial media query match status
+    setIsMobile(mediaQuery.matches);
 
+    // Define a callback function to handle changes in the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
 
-  // Define a callback function to handle changes in the media query
-  const handleMediaQueryChange = (event) => {
-    setIsMobile(event.matches);
-  }
+    // Add an event listener to the media query to detect changes
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
 
-// Add an event listener to the media query to detect changes
-  mediaQuery.addEventListener('change',
-   handleMediaQueryChange); 
-
-
-  // Clean up the event listener when the component unmounts
-   return () => {
-    mediaQuery.removeEventListener('change',
-     handleMediaQueryChange);
-   }
-}, [])
-  // The empty dependency array [] ensures that this effect runs only once after the initial render
-
-
+    // Clean up the event listener when the component unmounts
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    };
+  }, []);
 
   return (
     <Canvas
-     frameloop="demand"
-     shadows
+      frameloop="demand"
+      shadows
       gl={{ preserveDrawingBuffer: true }}
-      camera={{ position: [20, 3, 5], fov: 25 }}>
+      camera={{ position: [20, 3, 5], fov: 25 }}
+    >
       <Suspense fallback={<CanvasLoader />}>
-      {/* the suspense is using the canvasloader as a fallback while computer is loading, once it
-       loads computer 3d loads and the fallback goes away */}
+        {/* The suspense is using the CanvasLoader as a fallback while the computer 3D model is loading */}
         <Preload all />
         <OrbitControls enableZoom={false} />
 
